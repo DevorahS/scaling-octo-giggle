c56e56a0bc3084a3aa3f6d697fe3b7cabe229e91
devorahe23@gmail.com <dspielman@blitstein.edu>
2018-07-29 15:04:51 -0500

    Add JavaScript file

---
+++ js/app.js
@@ -0,0 +1,150 @@
+// document.write("Hello World!!!");
+// console.log("Hi this Devorah");
+
+// var name = "Devorah";
+// console.log(name);
+
+// var greeting = "Hello"+ " " + "world";
+// console.log(greeting);
+// console.log(greeting.length);
+// console.log(greeting.toLowerCase());
+// console.log(greeting.includes('e'));
+// console.log(greeting.indexOf('e'));
+// console.log(greeting.substring(1, 4));
+
+// var name = prompt("Tell me your name");
+// console.log(name)
+// 	document.write("<h1>Hello," + name + ", Welcome</h1>");
+
+// var number =4;
+// console.log(number + 2);
+
+// function sayHello(name) {
+// 	console.log("hello, " + name);
+// }
+// sayHello("Devorah");
+
+
+// function addTwoNumbers(num1, num2){
+// 	return num1 + num2
+// }
+
+
+// function add(){
+//  var num1 = parseInt( prompt('please input a number'));
+//  var num2 = parseInt(prompt (" now input another number"));
+//  var sum = addTwoNumbers (num1, num2);
+//  console.log(sum);
+// }
+// add();
+ 
+// function fullname(){
+// 	var firstname =  prompt( "enter your first name");
+// 	var lastname =  prompt("eneter your last name");
+// 	var sum = firstname + lastname
+// 	console.log(sum)
+
+// }
+// fullname();
+
+// function increment(){
+// 	var num1= parseInt(prompt " enter a number");
+// 	var sum= num1+1;
+// 	console.log(sum); 
+
+// }
+// increment();
+// increment();
+// increment();
+// increment();
+// increment();
+
+ function increment (number){
+// 	number += 1;
+// 	console.log(number);
+// 	number += 1;
+// 	console.log(number);
+// 	number += 1;
+// 	console.log(number);
+// 	number += 1;
+// 	console.log(number);
+// 	number += 1;
+// 	console.log(number);
+
+// }
+// increment(5)
+
+// if ("hey" ==== "Hey"){
+// 	console.log ("this is true");
+// }
+// else if( 4===4){
+// 	console.log("true");
+// }
+// else{
+// 	console.log("this is false");
+// }
+
+// function getNumber(){
+// 	var number = parseInt(prompt("Please enter a 1 or a 2!"))
+// 	if (number===2|| number ===1){
+// 		console.log("Thanks for the " + number + "!");
+// 	}
+// 	else{
+// 		console.log("thats not  a 1 or a 2 ");
+// 	}
+// }
+
+// getNumber()
+
+// function checkUser(){
+//   var userPhrase = prompt('Enter Knock Knock or knock knock')
+//   if (userPhrase==="Knock Knock" || userPhrase=== "knock knock"){
+// 	console.log("Who is there?");
+//  }
+// }
+//  checkUser();
+
+ // function takeNumber(){
+ // 	var number = parseInt(prompt("eneter a number"))
+ // 	if (number %2===0){
+ // 		console.log("Even");
+ // 	}
+ // 	else {
+ // 		console.log("Odd");
+ // 	}
+ // }
+ // takeNumber()
+
+ while (4 === 3){
+ 	console.log('True');
+
+ }
+
+ function logNumbers(number){
+ 	var counter = 1
+ 	while ( counter <= number){
+ 		console.log(counter);
+ 		counter+=1;
+
+ 	}
+ }
+ // logNumbers(10);
+
+for (var counter=0; counter<= 10; counter += 1){
+	console.log(counter)
+}
+
+function increment (number){
+for(var i=1; i<= number; i+=1){
+
+}console.log(i);
+	} 
+increment(10)
+
+function letters(string){
+	var string='Hello'
+	while (string.length < length)
+		console.log(string);
+		string+=1
+}
+letters();
